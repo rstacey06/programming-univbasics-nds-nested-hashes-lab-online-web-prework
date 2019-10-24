10dbc8f6b4a7cb713a36da841cb45b1508a74fda
burbage stacey <rstacey06@gmail.com>
2019-10-24 16:35:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,7 @@ def assembled_aoh
 end
 
 def literal_aoh
- [{:name =>"Don Gately", :occupation => "Live-in Staff", :name =>"Joelle van Dyne",}]
+ [{:name =>"Don Gately", :occupation => "Live-in Staff"}, {:name =>"Joelle van Dyne", :occupation => "Radio Personality"}, {:name => "Pat Monteseian", :occupation => "Staff" }, ]
 end
 
 def aoh_lookup(aoh, row, key)
